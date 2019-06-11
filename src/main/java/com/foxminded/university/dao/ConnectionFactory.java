74aa88674bc215cdb35984dee3319a4913091e0d
UrianskyiAnatolii <tolik.hazak@gmail.com>
2019-06-11 21:53:40 +0300

    Spring IoC 3

--- src/main/java/com/foxminded/university/dao/ConnectionFactory.java
+++ src/main/java/com/foxminded/university/dao/ConnectionFactory.java
@@ -2,13 +2,14 @@ package com.foxminded.university.dao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.BeansException;
-
-import com.foxminded.university.constants.Constants;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ConnectionFactory {
     
@@ -22,9 +23,10 @@ public class ConnectionFactory {
         try {
             log.trace("Checking DataSource on null");
             if (dataSource == null) {
-                
+                log.debug("Creating ApplicationContext(spring)");
+                ApplicationContext context = new ClassPathXmlApplicationContext("appContextSpring.xml");
                 log.debug("Getting DataSource from ApplicationContext(Spring)");
-                dataSource = (DataSource) Constants.CONTEXT_SPRING.getBean("dataSource", DataSource.class);
+                dataSource = (DataSource) context.getBean("dataSource", DataSource.class);
             }
             log.debug(" Getting connection from DataSource");
             connection = dataSource.getConnection();
